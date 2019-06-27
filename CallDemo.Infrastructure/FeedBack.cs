3b2175cc3694a75376202972e146f6d7feca8a20
wkq <172137365@qq.com>
2019-06-27 11:09:19 +0800

    添加demo项目文件

---
+++ CallDemo.Infrastructure/FeedBack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallDemo.Infrastructure
+{
+    public class FeedBack
+    {
+        public int ThetaAngle;
+        public int PhiAngle;
+        public double ThetaPower;
+        public double PhiPower;
+    }
+}
---
+++ CallDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CallDemo.Infrastructure;
+
+namespace CallDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //实例化一个底层类
+            Operation opt = new Operation(1);
+            //注册消息反馈事件
+            opt.OnDataArrived += Opt_OnDataArrived;
+            //初始化
+            opt.Init();
+            opt.StartTest();
+            Console.ReadLine();
+        }
+
+        //底层触发事件后上层可以在注册的事件中得到
+        private static void Opt_OnDataArrived(FeedBack data)
+        {
+            Console.WriteLine("ThetaPower = " + data.ThetaPower.ToString());
+            Console.WriteLine("PhiPower = " + data.PhiPower.ToString());
+        }
+    }
+}
